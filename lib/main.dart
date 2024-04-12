935084fc35f8773965d517a110431c0388f7f70b
morieeeenyo <uint2435@gmail.com>
2024-04-13 08:46:25 +0900

    chore: stateの更新

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      print("HelloWorld");
     });
   }
 
@@ -114,6 +113,37 @@ class _MyHomePageState extends State<MyHomePage> {
             size: 36.0,
           ),
         ]),
+        Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        if (_counter % 2 == 0)
+          const Text('偶数です', style: TextStyle(fontSize: 20, color: Colors.red)),
       ]),
       // body: Center(
       //   // Center is a layout widget. It takes a single child and positions it
@@ -145,7 +175,10 @@ class _MyHomePageState extends State<MyHomePage> {
       //   ),
       // ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () => {print("押したね？")}, child: const Icon(Icons.timer)),
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
       drawer: const Drawer(child: Center(child: Text("Drawer"))),
       endDrawer: const Drawer(
           child: Center(
