399d0a2d12b5c66fd4ca6ff85a1ea04cd39828d2
morieeeenyo <uint2435@gmail.com>
2024-04-23 06:55:44 +0900

    chore: navigator

---
+++ lib/test_page2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TestPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Test2"),
+        ),
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          TextButton(
+              onPressed: () => {Navigator.pushNamed(context, "/test3")},
+              child: const Text("進む", style: TextStyle(fontSize: 80))),
+          TextButton(
+              onPressed: () => {Navigator.of(context).pop()},
+              child: const Text("戻る", style: TextStyle(fontSize: 80)))
+        ])));
+  }
+}
