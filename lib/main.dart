0a291bc7695e673add2a4da2643e7a810c693279
morieeeenyo <uint2435@gmail.com>
2024-04-27 05:50:13 +0900

    chore: gps

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,88 +21,67 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  VideoPlayerController? _controller;
-  XFile? _image;
-  final imagePicker = ImagePicker();
+  String _latitude = "NoData";
+  String _longitude = "NoData";
+  String _altitude = "NoData";
+  String _distanceInMeters = "NoData";
+  String _bearing = "NoData";
 
-  // カメラから写真を取得するメソッド
-  Future getImageFromCamera() async {
-    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
-    setState(() {
-      if (pickedFile != null) {
-        _image = XFile(pickedFile.path);
-      }
-    });
-  }
-
-  // ギャラリーから写真を取得するメソッド
-  Future getImageFromGarally() async {
-    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      if (pickedFile != null) {
-        _image = XFile(pickedFile.path);
-      }
-    });
-  }
-
-  // カメラから動画を取得するメソッド
-  Future getVideoFromCamera() async {
-    XFile? pickedFile = await imagePicker.pickVideo(source: ImageSource.camera);
-    if (pickedFile != null) {
-      _controller = VideoPlayerController.file(File(pickedFile.path));
-      _controller!.initialize().then((_) {
-        setState(() {
-          _controller!.play();
-        });
-      });
+  Future<void> getLocation() async {
+    // 権限を取得
+    LocationPermission permission = await Geolocator.requestPermission();
+    // 権限がない場合は戻る
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
     }
-  }
-
-  // ギャラリーから動画を取得するメソッド
-  Future getVideoFromGarally() async {
-    XFile? pickedFile =
-        await imagePicker.pickVideo(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      _controller = VideoPlayerController.file(File(pickedFile.path));
-      _controller!.initialize().then((_) {
-        setState(() {
-          _controller!.play();
-        });
-      });
-    }
+    // 位置情報を取得
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      // 北緯がプラス、南緯がマイナス
+      _latitude = "緯度: ${position.latitude.toStringAsFixed(2)}";
+      // 東経がプラス、西経がマイナス
+      _longitude = "経度: ${position.longitude.toStringAsFixed(2)}";
+      // 高度
+      _altitude = "高度: ${position.altitude.toStringAsFixed(2)}";
+      // 距離を1000で割ってkmで返す(サンパウロとの距離)
+      _distanceInMeters =
+          "距離:${(Geolocator.distanceBetween(position.latitude, position.longitude, -23.61, -46.40) / 1000).toStringAsFixed(2)}";
+      // 方位を返す(サンパウロとの方位)
+      _bearing =
+          "方位: ${(Geolocator.bearingBetween(position.latitude, position.longitude, -23.61, -46.40)).toStringAsFixed(2)}";
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-            // 取得した写真を表示(ない場合はメッセージ)
-            child: _image == null
-                ? Text(
-                    '写真を選択してください',
-                    style: Theme.of(context).textTheme.headline4,
-                  )
-                : Image.file(File(_image!.path))),
-        floatingActionButton:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          // カメラから取得するボタン
-          FloatingActionButton(
-              onPressed: getImageFromCamera,
-              child: const Icon(Icons.photo_camera)),
-          // ギャラリーから取得するボタン
-          FloatingActionButton(
-              onPressed: getImageFromGarally,
-              child: const Icon(Icons.photo_album))
-        ]));
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_latitude, style: Theme.of(context).textTheme.headline4),
+              Text(_longitude, style: Theme.of(context).textTheme.headline4),
+              Text(_altitude, style: Theme.of(context).textTheme.headline4),
+              Text(_distanceInMeters,
+                  style: Theme.of(context).textTheme.headline4),
+              Text(_bearing, style: Theme.of(context).textTheme.headline4),
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: getLocation, child: const Icon(Icons.location_on)),
+    );
   }
 }
